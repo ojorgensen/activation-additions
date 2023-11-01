f90e2ebb62d0a7f4e8a4a92ef82188ae9add7ba7
ojorgensen <okj22@ic.ac.uk>
2023-11-01 18:11:14 +0000

    Made some small changes

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -13,7 +13,7 @@ def load_gpt_model_and_tokeniser(model_name: str, device: str = 'cuda'):
         #  Load the model form huggingface
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         tokenizer.pad_token = tokenizer.eos_token
-        model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+        model = AutoModelForCausalLM.from_pretrained(model_name)
 
         MODEL_CONFIG={"n_heads":model.config.n_head,
                       "n_layers":model.config.n_layer,
@@ -25,7 +25,7 @@ def load_gpt_model_and_tokeniser(model_name: str, device: str = 'cuda'):
     elif 'gpt-j' in model_name.lower():
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         tokenizer.pad_token = tokenizer.eos_token
-        model = AutoModelForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True).to(device)
+        model = AutoModelForCausalLM.from_pretrained(model_name, low_cpu_mem_usage=True)
 
         MODEL_CONFIG={"n_heads":model.config.n_head,
                       "n_layers":model.config.n_layer,
@@ -33,9 +33,50 @@ def load_gpt_model_and_tokeniser(model_name: str, device: str = 'cuda'):
                       "name_or_path":model.config.name_or_path,
                       "attn_hook_names":[f'transformer.h.{layer}.attn.out_proj' for layer in range(model.config.n_layer)],
                       "layer_hook_names":[f'transformer.h.{layer}' for layer in range(model.config.n_layer)]}
+    
+    elif 'gpt-neox' in model_name.lower():
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        tokenizer.pad_token = tokenizer.eos_token
+        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+
+        MODEL_CONFIG={"n_heads":model.config.num_attention_heads,
+                      "n_layers":model.config.num_hidden_layers,
+                      "resid_dim": model.config.hidden_size,
+                      "name_or_path":model.config.name_or_path,
+                      "attn_hook_names":[f'gpt_neox.layers.{layer}.attention.dense' for layer in range(model.config.num_hidden_layers)],
+                      "layer_hook_names":[f'gpt_neox.layers.{layer}' for layer in range(model.config.num_hidden_layers)]}
         
-    elif model_name == 'llama':
-        raise NotImplementedError
+    elif 'llama' in model_name.lower():
+        if '70b' in model_name.lower():
+            # use quantization. requires `bitsandbytes` library
+            from transformers import BitsAndBytesConfig
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=True,
+                bnb_4bit_quant_type='nf4',
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_compute_dtype=torch.float16
+            )
+            tokenizer = LlamaTokenizer.from_pretrained(model_name)
+            model = LlamaForCausalLM.from_pretrained(
+                    model_name,
+                    trust_remote_code=True,
+                    quantization_config=bnb_config
+            )
+        else:
+            if '7b' in model_name.lower():
+                model_dtype = torch.float32
+            else: #half precision for bigger llama models
+                model_dtype = torch.float16
+            tokenizer = LlamaTokenizer.from_pretrained(model_name)
+            model = LlamaForCausalLM.from_pretrained(model_name, torch_dtype=model_dtype)
+
+        MODEL_CONFIG={"n_heads":model.config.num_attention_heads,
+                      "n_layers":model.config.num_hidden_layers,
+                      "resid_dim":model.config.hidden_size,
+                      "name_or_path":model.config._name_or_path,
+                      "attn_hook_names":[f'model.layers.{layer}.self_attn.o_proj' for layer in range(model.config.num_hidden_layers)],
+                      "layer_hook_names":[f'model.layers.{layer}' for layer in range(model.config.num_hidden_layers)]}
+    
     else:
         raise NotImplementedError
     
