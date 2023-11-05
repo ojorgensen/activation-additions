1c98cda88ef4f21e1beca8b6c5674c10eeae3e36
ojorgensen <ojorgensen1417@gmail.com>
2023-11-05 18:51:08 +0000

    More progress on extracting activations

--- src/utils/extract_utils.py
+++ src/utils/extract_utils.py
@@ -3,6 +3,7 @@ import os, re, json
 from typing import List, Dict
 
 import torch
+import einops
 import numpy as np
 import pandas as pd
 from baukit import TraceDict
@@ -27,8 +28,8 @@ def gather_activations(prompt, layers, model, tokenizer):
     inputs = tokenizer(sentence, return_tensors='pt').to(model.device)
 
     # Access Activations 
-    with TraceDict(model, layers=layers, retain_input=True, retain_output=False) as td:                
-        model(**inputs) # batch_size x n_tokens x vocab_size, only want last token prediction
+    with TraceDict(model, layers=layers, retain_input=False, retain_output=True) as td:                
+        model(**inputs) # batch_size x n_tokens x activation_dim, only want last token prediction
 
     return td
 
@@ -46,13 +47,20 @@ def gather_activations_from_dataset(
     Collects all desired activations for arbitrary prompts, for a given type of activation.
     Returns a dictionary of tensors, with keys corresponding to the activation types.
     Arguments:
-    dataset: list of strings to be passed to model
-    activation_types: list of activation types to be collected
-    model: huggingface model
-    tokenizer: huggingface tokenizer
-    model_config: dictionary of model configuration
-    N_TRIALS: number of samples to collect
-    split_attention: whether to split attention tensors into heads
+        dataset: list of strings to be passed to model
+        activation_types: list of activation types to be collected
+        model: huggingface model
+        tokenizer: huggingface tokenizer
+        model_config: dictionary of model configuration
+        N_TRIALS: number of samples to collect
+        split_attention: whether to split attention tensors into heads
+        final_activations_only: whether to only collect the final activations
+    Returns:
+        activation_storage: dictionary of activations, with keys corresponding to activation types
+            This gives a dictionary with keys corresponding to each prompt. The value of each of these
+            is a tensor of activations, with shape (n_layers n_tokens hidden_size). If final_activations_only
+            is True, then the shape is (n_layers 1 hidden_size).
+
     """
     def split_activations_by_head(activations, model_config):
         """
@@ -72,11 +80,11 @@ def gather_activations_from_dataset(
     all_activation_layers = []
     for activation_type in activation_types:
         all_activation_layers += model_config[activation_type]
-        activation_storage[activation_type] = torch.zeros(N_TRIALS, len(model_config[activation_type]), model_config['resid_dim'])
+        activation_storage[activation_type] = {}
 
     for n in range(N_TRIALS):
         prompt = dataset[n]
-        if prepend_bos:
+        if is_llama:
             # work out how to setup prompts for llama
             raise NotImplementedError
         activations_td = gather_activations(prompt, all_activation_layers, model, tokenizer)
@@ -84,14 +92,20 @@ def gather_activations_from_dataset(
             raise NotImplementedError
         
         for activation_type in activation_types:
-            stack = torch.vstack([activations_td[layer].output[0] for layer in model_config[activation_type]])
+            print(activations_td[model_config[activation_type][0]].output.shape)
+            stack = torch.stack([activations_td[layer].output[0] for layer in model_config[activation_type]])
+            # Shape is (n_layers, n_tokens, hidden_size)
+            print(stack.shape)
             if final_activations_only:
                 stack = stack[:,-1,:]
+                stack = einops.rearrange(stack, 'n_layers hidden_size -> n_layers 1 hidden_size')
             else:
                 raise NotImplementedError
                 # Different activation shapes could be an issue here!
         
             activation_storage[activation_type][n] = stack
+    
+    return activation_storage
 
         
 
