81d8b4c5ed5dec44f38f40e147445999cd29c394
ojorgensen <ojorgensen1417@gmail.com>
2023-11-28 23:33:00 +0000

    More experiments

--- experiment_run.sh
+++ experiment_run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-python -m src.compute_avg_hidden_state --dataset_name=antonym --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=antonym --layer=12
 
-python -m src.compute_avg_hidden_state --dataset_name=capitalize --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=capitalize --layer=12
 
-python -m src.compute_avg_hidden_state --dataset_name=country-capital --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=country-capital --layer=12
 
-python -m src.compute_avg_hidden_state --dataset_name=english-french --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=english-french --layer=12
 
-python -m src.compute_avg_hidden_state --dataset_name=present-past --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=present-past --layer=12
 
-python -m src.compute_avg_hidden_state --dataset_name=singular-plural --layer=9
+python -m src.compute_avg_hidden_state --dataset_name=singular-plural --layer=12
 
-# Repeat for llama 7B
-python -m src.compute_avg_hidden_state --dataset_name=antonym --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# # Repeat for llama 7B
+# python -m src.compute_avg_hidden_state --dataset_name=antonym --model_name=meta-llama/Llama-2-7b-hf --layer=8
 
-python -m src.compute_avg_hidden_state --dataset_name=capitalize --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# python -m src.compute_avg_hidden_state --dataset_name=capitalize --model_name=meta-llama/Llama-2-7b-hf --layer=8
 
-python -m src.compute_avg_hidden_state --dataset_name=country-capital --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# python -m src.compute_avg_hidden_state --dataset_name=country-capital --model_name=meta-llama/Llama-2-7b-hf --layer=8
 
-python -m src.compute_avg_hidden_state --dataset_name=english-french --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# python -m src.compute_avg_hidden_state --dataset_name=english-french --model_name=meta-llama/Llama-2-7b-hf --layer=8
 
-python -m src.compute_avg_hidden_state --dataset_name=present-past --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# python -m src.compute_avg_hidden_state --dataset_name=present-past --model_name=meta-llama/Llama-2-7b-hf --layer=8
 
-python -m src.compute_avg_hidden_state --dataset_name=singular-plural --model_name=meta-llama/Llama-2-7b-hf --layer=8
+# python -m src.compute_avg_hidden_state --dataset_name=singular-plural --model_name=meta-llama/Llama-2-7b-hf --layer=8
